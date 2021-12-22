965144b6bf701c769a01cf5421f0d71f7dc3d00f
zhongchuan <zhongchuan909@gmail.com>
2021-12-22 10:50:56 +0900

    重写了get post方法 并且接收到前台的值

--- YGCbook/src/co/jp/netwisdom/servlet/userInfoRegServlet.java
+++ YGCbook/src/co/jp/netwisdom/servlet/userInfoRegServlet.java
@@ -1,61 +1,37 @@
 package co.jp.netwisdom.servlet;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import cn.key.Tools.DataTool;
-import cn.key.dao.BookInfoDAO;
-import cn.key.entity.BookInfo;
+
+ 
 
 public class userInfoRegServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		this.doPost(request, response);
+		//从前台接收到注册页面客户输入的信息
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String sex = req.getParameter("sex");
+		String major = req.getParameter("major");
+		String intro = req.getParameter("intro");
+		String[] hobbyArray = req.getParameterValues("hobby");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.doGet(req, resp);
 	}
 
 	
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String bookName = request.getParameter("bookName");
-		String pbdate = request.getParameter("pbdate");
-		String pbName = request.getParameter("pbName");
-		String author = request.getParameter("author");
-		String context = request.getParameter("context");
-		String smallImg = request.getParameter("smallImg");
-		String bigImg = request.getParameter("bigImg");
-		int booktypeId = Integer.parseInt(request.getParameter("booktypeId"));
-		int bookStates = Integer.parseInt(request.getParameter("bookStates"));
-		float price = Float.parseFloat(request.getParameter("price"));
-		float ygcprice = Float.parseFloat(request.getParameter("ygcprice"));
-		BookInfoDAO dao = new BookInfoDAO();
-		
-		Date date = null;
-		try {
-			date = DataTool.stringtoDate(pbdate);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		BookInfo book = new BookInfo(bookName, booktypeId, pbName, author, context, smallImg, bigImg, price, date, bookStates, ygcprice);
-		String path = "/Error.jsp";
-		String returnPath = "/background/bookInfo/SaveBookInfo.jsp";
-		if (dao.save(book)) {
-			path = "/Success.jsp";
-			returnPath = "/FindAllBookInfoServlet";
-		}
-		request.setAttribute("title","����");
-		request.setAttribute("returnPath",returnPath);
-		RequestDispatcher rd =  request.getRequestDispatcher("/background"+ path);
-		rd.forward(request, response);
-		
-		
 	}
 
-}
+	
+	
