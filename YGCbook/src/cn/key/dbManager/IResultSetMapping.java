bc602cea91f0c8b873e44fa9308abcbd2c3d1b48
zhongchuan <zhongchuan909@gmail.com>
2022-08-03 21:07:47 +0900

    ji

--- YGCbook/src/cn/key/dbManager/IResultSetMapping.java
+++
@@ -1,8 +0,0 @@
-package cn.key.dbManager;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface IResultSetMapping {
-	public Object mapping(ResultSet rs) throws SQLException ;
-}
